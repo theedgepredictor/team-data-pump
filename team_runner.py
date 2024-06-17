7c7f520198c72ad4d8a280459b803c25af57facf
Charlie Sergeant <theedgepredictor@gmail.com>
2024-06-17 19:16:02 -0400

    prep

---
+++ team_runner.py
@@ -0,0 +1,7 @@
+import json
+import os
+
+from espn_api_orm.league.api import ESPNLeagueAPI
+from espn_api_orm.season.api import ESPNSeasonAPI
+from espn_api_orm.team.api import ESPNTeamAPI
+from espn_api_orm.consts import ESPNSportLeagueTypes
